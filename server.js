c292a73824a418bfee2067b309ad6a0f39aab308
Alex Davey <alexdavey0@googlemail.com>
2011-08-27 09:41:12 +0800

    added express

--- server.js
+++ server.js
@@ -1,12 +1,39 @@
-// server.js 
-var http = require('http') 
-, nko = require('nko')('VCPo4hn9tsswPvB7');
 
-var app = http.createServer(function (req, res) { 
-res.writeHead(200, { 'Content-Type': 'text/html' }); 
-res.end('Hello, World'); 
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+	nko = require('nko')('VCPo4hn9tsswPvB7');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
 });
 
-app.listen(parseInt(process.env.PORT) || 7777); 
-console.log('Listening on ' + app.address().port);
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
 
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', function(req, res){
+  res.render('index', {
+    title: 'Express'
+  });
+});
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
