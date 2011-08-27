c61c3222d37c0d5dd832daf2c2c994976c575bf4
Koen Klaren <koen.klaren@gmail.com>
2011-08-27 17:33:05 +0200

    Fixing a mis-merge

--- app.js
+++ app.js
@@ -22,10 +22,10 @@ var db = {
 }
 
 function unique(charset, length, number) {
-    var base = strlen(charset), converted = "";
+    var base = charset.length, converted = "";
     while (length > 0) {
-        converted = charset[length % base] + converted;
-        length = ~~(length / base);
+	converted = charset[length % base] + converted;
+	length = ~~(length / base);
     }
     return converted;
 }
