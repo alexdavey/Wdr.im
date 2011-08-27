978db514113c4acabd95eeaeef0d4de043d9c7be
Koen Klaren <koen.klaren@gmail.com>
2011-08-27 20:16:54 +0200

    Completed the main info part of the tracking page

--- app.js
+++ app.js
@@ -14,32 +14,32 @@ var express  = require('express'),
 // =============================================================================
 
 var id = 1,
-	charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-=";
+    charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-=";
 
 var db = {
-	setId : function() {return}
+    setId : function() {return}
 }
 
-function unique(charset, length, number) {
-	var base = charset.length, converted = '';
-	do {
-		converted = charset[number % base] + converted;
-		number = Math.floor(number / base);
-	} while (number > 0)
-	return converted;
+function unique(charset, number) {
+    var base = charset.length, converted = '';
+    do {
+        converted = charset[number % base] + converted;
+        number = Math.floor(number / base);
+    } while (number > 0);
+    return converted;
 }
 
 
 function getIp(req) {
-	var ipAddress, forwardedIpsStr = req.header('x-forwarded-for'); 
-	if (forwardedIpsStr) {
-		var forwardedIps = forwardedIpsStr.split(',');
-		ipAddress = forwardedIps[0];
-	}
-		if (!ipAddress) {
-		ipAddress = req.connection.remoteAddress;
-	}
-	return ipAddress;
+    var ipAddress, forwardedIpsStr = req.header('x-forwarded-for'); 
+    if (forwardedIpsStr) {
+        var forwardedIps = forwardedIpsStr.split(',');
+        ipAddress = forwardedIps[0];
+    }
+    if (!ipAddress) {
+        ipAddress = req.connection.remoteAddress;
+    }
+    return ipAddress;
 }
 
 // =============================================================================
@@ -49,49 +49,54 @@ function getIp(req) {
 var app = module.exports = express.createServer();
 
 app.configure(function(){
-	app.set('views', __dirname + '/views');
-	app.set('view engine', 'ejs');
-	app.use(express.bodyParser());
-	app.use(express.methodOverride());
-	app.use(app.router);
-	app.use(express.static(__dirname + '/public'));
+    app.use(express.static(__dirname + '/public'));
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
 });
 
 app.configure('development', function(){
-	app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
 app.configure('production', function(){
-	app.use(express.errorHandler()); 
+    app.use(express.errorHandler()); 
 });
 
 // =============================================================================
 // |                                 Routes  								   |
 // =============================================================================
 app.get('/', function(req, res){
-	res.render('index');
+    res.render('index', {view:'index'});
 });
 
 
-app.get(/\/([0-9a-z\_\-\=]{1, 6})\+/i, function(req, res) {
-	res.render('track', {
-		id : req.params[0]
-	});
+app.get(/\/([0-9a-z\_\-\=]{1,6})\+/i, function(req, res) {
+    res.render('track', {
+        id      : req.params[0],
+        longUrl : 'http://example.com',
+        clicks  : 7071,
+        
+        view    : 'tracking'
+    });
 });
 
 // API routes
-app.get(/\/([0-9a-z\_\-\=]{1, 6})/i, function(req, res) {
-	var id = req.params[0];
+app.get(/\/([0-9a-z\_\-\=]{1,6})/i, function(req, res) {
+    var id = req.params[0];
 });
 
 app.post(/\/data/, function(req, res) {
-	if (!req.body.url) res.send('Error, Error!');
-	var shortUrl = unique(charset, id++);
-	res.redirect('/' + shortUrl + '+');
-	db.setId(shortUrl, {
-		longUrl : req.body.url,
-		startTime : new Date()
-	});
+    if (!req.body.url)
+        res.send('Error, Error!');
+    var shortUrl = unique(charset, id++);
+    res.redirect('/' + shortUrl + '+');
+    db.setId(shortUrl, {
+        longUrl : req.body.url,
+        startTime : new Date()
+    });
 });
 
 
