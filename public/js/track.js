7890a0caf2dc1b4e6bd1e9bfced603d954371941
Koen Klaren <koen.klaren@gmail.com>
2011-08-29 00:34:11 +0200

    Client side Socket.IO start

--- public/js/track.js
+++ public/js/track.js
@@ -1,51 +1,3 @@
-<<<<<<< HEAD
-var wdr = new (function(){
-    this.ready = function(){
-	// Animate the colored strip
-	var stripi = 0;
-	var animateStrip = function(){
-	    strip.tween('background-position', (++stripi * 1000) + 'px 0px');
-	};
-	var strip = $('color-strip').setStyle('background-position', '0px 0px')
-				    .set('tween', {
-	    duration: 	10000,
-	    transition: Fx.Transitions.linear,
-	    link: 	'cancel',
-	    onComplete:	animateStrip
-	});
-	animateStrip();
-	
-	if ($('shorten-url')){
-	    $('shorten-url').addEvent('submit', function(e){	    
-		var url = this.getElement('input'),
-		    hasProtocol = /^(http(?:s)?\:\/\/)/,
-		    isUrl = /^(http(?:s)?\:\/\/[a-zA-Z0-9\-]+(?:\.[a-zA-Z0-9\-]+)*\.[a-zA-Z]{2,6}(?:\/?|(?:\/[\w\-]+)*)(?:\/?|\/\w+\.[a-zA-Z]{2,4}(?:\?[\w]+\=[\w\-]+)?)?(?:\&[\w]+\=[\w\-]+)*)$/;
-		if (!isUrl.test(url.value)){
-		    if (!hasProtocol.test(url.value)){
-			url.value = 'http://' + url.value;
-			if (!isUrl.test(url.value)){
-			    e.preventDefault();
-			    wdr.flash('#FF0000', 2000);
-			}
-		    } else {
-			e.preventDefault();
-			wdr.flash('#FF0000', 2000);
-		    }
-		}
-	    });
-	}
-    }
-    
-    this.flash = function(color, time){
-	var el = $('color-flash').setStyle('background-color', color).fade(1);
-	setTimeout(function(){
-	    el.fade(0);
-	}, time);
-    };
-    
-    // Onload
-    window.addEvent('domready', this.ready);
-=======
 var track = new (function(){
     
     // Change the time range
@@ -145,10 +97,9 @@ var track = new (function(){
     });
     
     var hits = [];
-    var socket = new io.Socket();
-    socket.connect();
+    var socket = socket.connect();
     socket.on('connect', function(){
-        socket.send(JSON.encode({id:window.pageID}));
+        socket.emit('id', window.pageID);
         
         socket.on('message', function(data){
             data = JSON.decode(data);
@@ -178,12 +129,11 @@ track.setChart('browser', {
     'FireFox'           : 30,
     'Safari'            : 10,
     'Opera'             : 7,
-    'Internet Explorer' : 13,
+    'Internet Explorer' : 13
 });
 
 track.setChart('os', {
     'Mac OSX'   : 20,
     'Windows'   : 70,
     'Ubuntu'    : 10
->>>>>>> cacc0e40857b382bca8c8ec5fabb352380347225
 });
