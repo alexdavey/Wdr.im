ab8d5ef65d6bf307f601ce606f0c966a4fe7b5a9
Koen Klaren <koen.klaren@gmail.com>
2011-08-28 12:37:16 +0200

    Added the map and graphs

---
+++ public/js/track.js
@@ -0,0 +1,78 @@
+var track = new (function(){
+    
+    this.mapsLoaded = function(){
+	var myLatlng = new google.maps.LatLng(20, 0);
+	var myOptions = {
+	    zoom: 2,
+	    center: myLatlng,
+	    mapTypeId: google.maps.MapTypeId.HYBRID
+	}
+	this.map.map = new google.maps.Map($("map"), myOptions);
+    }
+    
+    this.map = {};
+    this.map.addMarker = function(lat, lng){
+	var latlng = new google.maps.LatLng(lat, lng);
+	var marker = new google.maps.Marker({
+	    position: latlng,
+	    map: this.map,
+	    zIndex: Math.round(latlng.lat()*-100000)<<5
+        });
+    }
+    
+    this.initCharts = function(){
+	$('charts').getChildren().each(function(chart){
+	    chart.store('r', Raphael(chart));
+	});
+    }
+    
+    this.setChart = function(name, data){
+	var r = $(name + '-chart').retrieve('r');
+        r.clear();
+	var pie = r.g.piechart(110, 110, 90, Object.values(data), {legend:Object.values(Object.map(data, function(val, key){return '%% - ' + key}))});
+        pie.hover(function () {
+            this.sector.stop();
+            this.sector.scale(1.1, 1.1, this.cx, this.cy);
+            if (this.label) {
+                this.label[0].stop();
+                this.label[0].scale(1.5);
+                this.label[1].attr({"font-weight": 800});
+            }
+        }, function () {
+            this.sector.animate({scale: [1, 1, this.cx, this.cy]}, 500, "bounce");
+            if (this.label) {
+                this.label[0].animate({scale: 1}, 500, "bounce");
+                this.label[1].attr({"font-weight": 400});
+            }
+        });
+    }
+    
+});
+
+track.initCharts();
+track.setChart('location', {
+    'United States'     : 60,
+    'United Kingdom'    : 20,
+    'Netherlands'       : 10,
+    'Candy Mountain'    : 10
+});
+
+track.setChart('referrer', {
+    'yahoo.com'             : 40,
+    'webdevrefinery.com'    : 45,
+    'nodeknockout.com'      : 15
+});
+
+track.setChart('browser', {
+    'Chrome'            : 40,
+    'FireFox'           : 30,
+    'Safari'            : 10,
+    'Opera'             : 7,
+    'Internet Explorer' : 13,
+});
+
+track.setChart('os', {
+    'Mac OSX'   : 20,
+    'Windows'   : 70,
+    'Ubuntu'    : 10
+});
